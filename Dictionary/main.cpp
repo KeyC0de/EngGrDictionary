b1934785cf88a9d11ae75f2fa00dd0a2461f3a1e
KeyC0de <nik.lazkey@gmail.com>
2020-11-13 13:41:45 +0200

    fixed unicode & Greek input, output for Windows

--- Dictionary/main.cpp
+++ Dictionary/main.cpp
@@ -1,23 +1,92 @@
 ﻿#include <clocale>
 #include <io.h>
 #include <fcntl.h>
-#include "digital_dictionary.hpp"
 #include <iostream>
+#include "winner.h"
+#include "digital_dictionary.hpp"
+
+
+#pragma region WINCONSOLE
+#if defined _WIN32 || defined _MSC_VER
+static DWORD getFontFamily( HANDLE h )
+{
+	CONSOLE_FONT_INFO cfi;
+	BOOL conFont = GetCurrentConsoleFont( h,
+		false,
+		&cfi );
+	std::wcout << conFont
+		<< L"\nnFont="
+		<< cfi.nFont
+		<< L"fontSize=("
+		<< cfi.dwFontSize.X
+		<< L','
+		<< cfi.dwFontSize.Y
+		<< L")\n";
+	return conFont ? cfi.nFont
+		: -1;
+}
+
+void getConsoleInfo( HANDLE h )
+{
+	using GETNUMBEROFCONSOLEFONTS = DWORD (WINAPI* )();
+    using SETCONSOLEFONT = BOOL (WINAPI*)( HANDLE hConOut, DWORD nFont );
+	auto GetNumberOfConsoleFonts =
+		(GETNUMBEROFCONSOLEFONTS) GetProcAddress( LoadLibraryW( L"KERNEL32" ),
+		"GetNumberOfConsoleFonts" );
+    auto SetConsoleFont = (SETCONSOLEFONT) GetProcAddress(LoadLibraryW(L"KERNEL32"),
+		"SetConsoleFont");
+	auto font = getFontFamily( h );
+	std::wcout << L"nConsoleFonts="
+		<< GetNumberOfConsoleFonts()
+		<< L"fontName="
+		<< font
+		<< L'\n';
+}
+#endif
+#pragma endregion
 
 int main() 
 {
 	fflush( stdout );
+#if defined _WIN32 || defined _MSC_VER
+	// change code page to UNICODE
+	if ( !IsValidCodePage( CP_UTF8 ) )
+	{
+		return GetLastError();
+	}
+	if ( !SetConsoleCP( CP_UTF8 ) )
+	{
+		return GetLastError();
+	}
+	if ( !SetConsoleOutputCP( CP_UTF8 ) )
+	{
+		return GetLastError();
+	}
+	
+	// change console font - post Windows Vista only
+	HANDLE hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+	CONSOLE_FONT_INFOEX cfie;
+	const auto sz = sizeof( CONSOLE_FONT_INFOEX );
+	ZeroMemory( &cfie, sz );
+	cfie.cbSize = sz;
+	cfie.dwFontSize.Y = 14;
+	wcscpy_s( cfie.FaceName,
+		L"Lucida Console" );
+	SetCurrentConsoleFontEx( hStdOut,
+		false,
+		&cfie );
+
+	getConsoleInfo( hStdOut );
+	
+	// change file translation mode
 	_setmode( _fileno( stdout ), _O_U16TEXT );
 	_setmode( _fileno( stderr ), _O_U16TEXT );
 	_setmode( _fileno( stdin ), _O_U16TEXT );
+	// use w-streams for interaction with the console
+	// use regular streams for interaction with files
+#endif
 	std::ios_base::sync_with_stdio( false );
-	// use w-streams for interaction with the console & regular streams for interaction with files
-	//const char* zsEnglishLocaleName = "en-US.UTF-8";
-	//const wchar_t* zwsEnglishLocaleName = L"en-US.UTF-8";
-	//std::setlocale( LC_ALL, zsEnglishLocaleName );
-	//std::locale englishLocale{ zsEnglishLocaleName };
-	//std::locale::global( englishLocale );
-	//_wsetlocale( LC_ALL, zwsEnglishLocaleName );
+
 
 	EngGrDictionary& inst = EngGrDictionary::getInstance();
 
